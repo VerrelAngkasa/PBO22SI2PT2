8d44f576bb58048cc8864ad9f9dff9d1d8a21851
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2023-05-27 14:31:12 +0700

    verel
    java

--- PBO/src/verre.java
+++ PBO/src/verre.java
@@ -1,6 +1,6 @@
-//Nama :Verrel
+//Nama : Verrel
 //NIM : 03081220031
-public class App {
+public class verre {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Nama saya Verrel");
     }
